8d3c28ffb9275575b339108ae5fc829769278241
Amber <415411212@qq.com>
2019-03-25 01:56:48 -0400

    Updated 2 levels.

---
+++ CodeLab1-HW5/Assets/Scripts/Goal.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class Goal : MonoBehaviour
+{
+    public Text Win;
+    void OnCollisionEnter(Collision other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            SceneManager.LoadScene("Scene2");
+        }
+    }
+}
---
+++ CodeLab1-HW5/Assets/Scripts/PlayerControl.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControl : MonoBehaviour
+{
+    public float MoveSpead;
+
+    public KeyCode leftKey;
+    public KeyCode rightKey;
+    public KeyCode upKey;
+    public KeyCode downKey;
+
+    public Rigidbody rb;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector2 newForce = new Vector2(0, 0); //the force we will add to our player
+
+
+        if (Input.GetKey(leftKey)) //When someone presses "A"
+        {
+            newForce.x -= MoveSpead;
+        }
+
+        if (Input.GetKey(rightKey)) //When someone presses "D"
+        {
+            newForce.x += MoveSpead;
+        }
+        
+        if (Input.GetKey(upKey)) //When someone presses "W"
+        {
+            newForce.y += MoveSpead;
+        }
+        
+        if (Input.GetKey(downKey)) //When someone presses "S"
+        {
+            newForce.y -= MoveSpead;
+        }
+
+        rb.AddForce(newForce);
+
+    }
+}
